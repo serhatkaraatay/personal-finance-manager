3fc277f7eabf1c96c7f67dc3ad3e4274349fd595
Serhat <mserhatkaraatay@gmail.com>
2025-10-27 02:37:09 +0300

    gelir ekleme eklendi

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList <String> gelirListesi= new ArrayList<String>();
         while (true) {
             System.out.println("------ Kişisel Finans Yönetim Sistemi ------");
             System.out.println("Seçenekler");
@@ -14,13 +16,26 @@ public class Main {
             int secim = scanner.nextInt();
             scanner.nextLine();
             if(secim == 1){
-                System.out.println("Gelir Ekleme Seçildi");
+                System.out.println("Gelir Miktarını Giriniz: ");
+                double gelir_miktar = scanner.nextDouble();
+                scanner.nextLine();
+                System.out.println("Gelir Türünü Giriniz (örn: maaş, harçlık) : ");
+                String gelir_turu =  scanner.nextLine();
+                String gelir = gelir_turu + " - " +  gelir_miktar + " TL";
+                gelirListesi.add(gelir);
             }
             else if(secim == 2){
                 System.out.println("Gider Ekleme Seçildi");
             }
             else if(secim == 3){
-                System.out.println("Tüm işlemleri Görüntüle Seçildi");
+                if(gelirListesi.isEmpty()){
+                    System.out.println("Gelir Listesi Şu An Boş! ");
+                }
+                else {
+                    for(String gelir : gelirListesi){
+                        System.out.println(gelir);
+                    }
+                }
             }
             else if (secim == 4){
                 System.out.println("Program sonlandırıldı");
